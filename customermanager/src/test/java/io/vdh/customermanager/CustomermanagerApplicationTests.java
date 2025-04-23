6bc30dc4e3d904d6f2801ba8dfb89a175a69a835
Vo Dang Huy <iamdaghuy@gmail.com>
2025-04-23 12:47:31 +0700

    first commit

---
+++ customermanager/src/test/java/io/vdh/customermanager/CustomermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vdh.customermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
